52b6311fffa2023e35e86fb692236a7c18bddb65
Oshermm <34840080+Oshermm@users.noreply.github.com>
2026-06-26 15:27:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
-from flask import Flask, request
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Bot is alive"
+    return "HOME OK", 200
+
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    return "HELLO FROM PYTHON", 200
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    return "WEBHOOK HIT", 200
