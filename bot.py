0c6aa68421d53fdf53b3562fc52e4a0593f4dc40
Oshermm <34840080+Oshermm@users.noreply.github.com>
2026-06-26 16:45:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
-from flask import Flask
-import sys
+from flask import Flask, request
+import os
+import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return "HOME OK", 200
+TOKEN = "PUT_YOUR_TOKEN_HERE"
+TG_API = f"https://api.telegram.org/bot{TOKEN}"
+
+def send_keyboard(chat_id):
+    keyboard = {
+        "inline_keyboard": [
+            [{"text": "🟢 קמה", "callback_data": "WAKE"}],
+            [{"text": "😴 הונחה לישון", "callback_data": "SLEEP"}]
+        ]
+    }
+
+    requests.post(f"{TG_API}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": "בחר פעולה:",
+        "reply_markup": keyboard
+    })
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    sys.stdout.write("🔥 WEBHOOK HIT FROM PYTHON\n")
-    sys.stdout.flush()
-    return "OK", 200
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        if text == "/start":
+            send_keyboard(chat_id)
+
+    return "ok", 200
