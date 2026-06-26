925950b2025a9ad0585f8e3cae97904052ec2e0a
Oshermm <34840080+Oshermm@users.noreply.github.com>
2026-06-26 17:35:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,13 +81,14 @@ def webhook():
             now = datetime.now()
         
             message = "🟢 קמה"
-            log_event("WAKE", sleep_duration)
         
             if last_sleep_time:
                 sleep_duration = now - last_sleep_time
                 message += f"\n😴 ישנה: {str(sleep_duration).split('.')[0]}"
         
             last_wake_time = now
+
+            log_event("WAKE", sleep_duration)
         
             requests.post(f"{TG_API}/sendMessage", json={
                 "chat_id": chat_id,
@@ -99,13 +100,14 @@ def webhook():
             now = datetime.now()
         
             message = "😴 הונחה לישון"
-            log_event("SLEEP", awake_duration)
         
             if last_wake_time:
                 awake_duration = now - last_wake_time
                 message += f"\n⏰ ערה: {str(awake_duration).split('.')[0]}"
         
             last_sleep_time = now
+
+            log_event("SLEEP", awake_duration)
         
             requests.post(f"{TG_API}/sendMessage", json={
                 "chat_id": chat_id,
